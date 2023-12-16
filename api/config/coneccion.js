dbd8a29dcea350b4714b444f9f73922037069416
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-12-16 09:45:14 -0400

    con mas from

--- api/config/coneccion.js
+++ api/config/coneccion.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+import { Sequelize } from 'sequelize';
 
 
 const sequelize = new Sequelize('postgres://graco:d16mVIlilx3OFVzXgb0AW5VYnTOv0pMT@dpg-clupqhmg1b2c73cacl4g-a.oregon-postgres.render.com/gracoapidb?ssl=true');
