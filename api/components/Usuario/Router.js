a36b73d12428744dfe37b9c77fc177092d528bd2
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-12-19 12:18:12 -0400

    con todo su router en usuario

--- api/components/Usuario/Router.js
+++ api/components/Usuario/Router.js
@@ -1,16 +1,19 @@
 import { validarCuerpo, validarId } from "./Validaciones.js";
+import { UsuarioControlador } from "./Controlador.js";
 
 const UsuarioRouter = (app) => {
 
-    app.get('/Usuario');
+    const controlador = new UsuarioControlador()
+
+    app.get('/Usuario', controlador.Listar);
     
-    app.get('/Usuario/:id', validarId)
+    app.get('/Usuario/:id', validarId, controlador.Consultar)
     
-    app.post('/Usuario', validarCuerpo);
+    app.post('/Usuario', validarCuerpo, controlador.Crear);
     
-    app.put('/Usuario/:id', validarId, validarCuerpo)
+    app.put('/Usuario/:id', validarId, validarCuerpo, controlador.Modificar)
     
-    app.delete('/Usuario/:id', validarId)
+    app.delete('/Usuario/:id', validarId, controlador.Eliminar)
     
 }
 
