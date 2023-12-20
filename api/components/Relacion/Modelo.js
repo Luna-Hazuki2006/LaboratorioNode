a51a54fedc86efd9b7efa5ddc2fef533d25d1046
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-12-19 21:49:48 -0400

    con relaciones buenas :D

---
+++ api/components/Relacion/Modelo.js
@@ -0,0 +1,35 @@
+import { DataTypes } from 'sequelize'; 
+import { sequelize } from '../../config/coneccion.js';
+import { Autores } from "../Autor/Modelo.js";
+import { TiposRelaciones } from "../TiposRelaciones/Modelo.js";
+
+const Relaciones = sequelize.define('Relaciones', {
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    descripcion: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }, 
+    idTiposRelaciones: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    }, 
+    idAutor: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    }, 
+    createdAt: {
+        type: DataTypes.DATE, 
+        defaultValue: DataTypes.NOW
+    }
+}, { tableName: 'relaciones' });
+
+TiposRelaciones.hasMany(Relaciones, {through: 'idTiposRelaciones'})
+Relaciones.belongsTo(TiposRelaciones)
+
+Autores.hasMany(Relaciones, {through: 'idAutor'})
+Relaciones.belongsTo(Autores)
+
+export {Relaciones}
