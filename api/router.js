c2e13f4c2ed4178a44455d5d40fe42e76c82b706
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-12-30 14:23:01 -0400

    llamado con el router

--- api/router.js
+++ api/router.js
@@ -2,7 +2,7 @@ import AutorRouter from './components/Autor/Router.js'
 import PublicacionRouter from './components/Publicacion/Router.js'
 // import SesionRouter from './components/Sesion/Router.js'
 // import MensajeRouter from "./components/Mensaje/Router.js";
-// import ConversacionRouter from "./components/Conversacion/Router.js";
+import ConversacionRouter from "./components/Conversacion/Router.js";
 import UsuarioRouter from './components/Usuario/Router.js'
 import TiposRelacionesRouter from "./components/TiposRelaciones/Router.js";
 import TipoUsuarioRouter from "./components/TipoUsuario/Router.js";
@@ -19,7 +19,7 @@ const router = (app) => {
     AutorRouter(app)
     // SesionRouter(app)
     // MensajeRouter(app)
-    // ConversacionRouter(app)
+    ConversacionRouter(app)
     PublicacionRouter(app)
     UsuarioRouter(app)
     TiposRelacionesRouter(app)
