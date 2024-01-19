f845ef35a41222efa8d8ff78f4d3b3698e75bf4f
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-01-19 18:01:57 -0400

    con estoooooooooooooooo :3

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import  express  from 'express';
 
 import { router } from './api/router.js'
 import { sequelize } from './api/config/coneccion.js';
+import swaggerjsdoc from 'swagger-jsdoc';
+import swaggerui from 'swagger-ui-express'
 
 const app = express();
 // y también le hice los cambios que me dijo de nombres de los archivos
@@ -14,6 +16,31 @@ const routerExpress = express.Router();
 router(routerExpress)
 
 app.use(routerExpress)
+
+const options = {
+    definition: {
+        openapi: "3.0.0", 
+        info: {
+            title: "Api para aplicación de venta de libros con Express y Swagger", 
+            version: "0.1.0", 
+            description: "Una api hecha en nodejs de venta de libros con Express y documentada con Swagger", 
+            contact: {
+                name: "Venta de libros online", 
+                url: "https://www.linkedin.com/in/ana-paula-mendoza-d%C3%ADaz-1a65b4250", 
+                email: "anapaulamendozadiaz2006@gmail.com"
+            }
+        }, 
+        servers: [
+            {
+                url: "http://localhost:9000/"
+            }
+        ]
+    },
+    apis: ["./api/router.js"]
+};
+
+const spacs = swaggerjsdoc(options)
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(spacs))
 
 const port = 9000;
 // :v profe... ¿y ahora qué hice? :'v
